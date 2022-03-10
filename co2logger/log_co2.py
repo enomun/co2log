cec259193a91e369cc59903ea77491c1f804d700
enomun <eno.mun.moon@gmail.com>
2022-03-11 05:24:40 +0900

    add sensor calibration command

--- co2logger/log_co2.py
+++ co2logger/log_co2.py
@@ -7,11 +7,13 @@ import time
 from database import DB
 from sensor import CO2Reader
 
+
 def create_parser(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("--dbpath", default="./co2.db")
-    parser.add_argument("--interval", type=int,default=10)
+    parser.add_argument("--interval", type=int, default=10)
     parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--calibrate", action="store_true")
 
     args = parser.parse_args(argv[1:])
     return args
@@ -19,7 +21,10 @@ def create_parser(argv):
 
 def main(args):
     dbcls = DB(args.dbpath)
-    co2sensor = CO2Reader(device="/dev/ttyS0",debug=args.debug)
+    co2sensor = CO2Reader(device="/dev/ttyS0", debug=args.debug)
+
+    if args.calibrate:
+        co2sensor.calibrate()
 
     while True:
         co2 = co2sensor.read()
@@ -27,10 +32,11 @@ def main(args):
             continue
         now = str(datetime.datetime.now())
         values = (now, co2)
-        print(values)    
+        print(values)
         dbcls.write(values)
         dbcls.commit()
         time.sleep(args.interval)
+
 
 if __name__ == "__main__":
     args = create_parser(sys.argv)
--- co2logger/sensor.py
+++ co2logger/sensor.py
@@ -1,25 +1,35 @@
 import serial
 import random
+
+
 class CO2Reader:
     """
     for MH-Z14
     """
+
     def __init__(self, device, debug=False):
         if not debug:
             self.s = serial.Serial(device, baudrate=9600, timeout=0.1)
+            self.stop_auto_calibration()
             self.read = self.read_co2
         else:
             self.read = self.read_dummy
 
-    
+    def stop_auto_calibration(self):
+        print("sending stop auto calibration command")
+        self.s.write(bytes([0xFF, 0x01, 0x79, 0x00, 0x00, 0x00, 0x00, 0x00, 0x86]))
+
+    def start_auto_calibration(self):
+        print("sending start auto calibration command")
+        self.s.write(bytes([0xFF, 0x01, 0x79, 0xA0, 0x00, 0x00, 0x00, 0x00, 0xE6]))
+
     def calibrate(self):
         print("sending calibration command")
         self.s.write(bytes([0xFF, 0x01, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, 0x78]))
         print("CO2 concentration after calibration: ", self.read_co2())
 
-
     def read_co2(self):
-        # Send command to 
+        # Send command to
         self.s.write(bytes([0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]))
         # Read response
         data = self.s.read(9)
@@ -49,4 +59,3 @@ class CO2Reader:
     def read_dummy(self):
         default = 400
         return default + random.random() * 50
-
