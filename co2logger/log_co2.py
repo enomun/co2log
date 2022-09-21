c0de56ecb6de319f783426c0d9b6bfabd99e5e43
enomun <eno.mun.moon@gmail.com>
2022-09-21 14:01:19 +0900

    refactor log_co2

--- co2logger/log_co2.py
+++ co2logger/log_co2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 import argparse
-import datetime
+from datetime import datetime
 import time
 
 from database import DB
@@ -11,7 +11,7 @@ from sensor import CO2Reader
 def create_parser(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("--dbpath", default="./co2.db")
-    parser.add_argument("--interval", type=int, default=10)
+    parser.add_argument("--interval", type=int, default=10, help="log interval [sec]")
     parser.add_argument("--debug", action="store_true")
     parser.add_argument("--calibrate", action="store_true")
 
@@ -30,7 +30,7 @@ def main(args):
         co2 = co2sensor.read()
         if not co2:
             continue
-        now = str(datetime.datetime.now())
+        now = str(datetime.now())
         values = (now, co2)
         print(values)
         dbcls.write(values)
